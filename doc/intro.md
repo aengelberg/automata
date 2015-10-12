d3ceecbf43fce782a3db9c0a56aee52a43e1f678
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2015-10-11 18:56:59 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to automata
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/automata/core_test.clj
@@ -0,0 +1,7 @@
+(ns automata.core-test
+  (:require [clojure.test :refer :all]
+            [automata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
